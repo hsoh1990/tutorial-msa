2e07c12d87f9b0a4587c240256a54a6f8a6df713
hsoh1990 <ohs4123@gmail.com>
2019-04-05 09:45:29 +0900

    api gateway zuul server 사용법

--- display-service/src/main/java/me/wellstone/msa/product/ProductRemoteService.java
+++ display-service/src/main/java/me/wellstone/msa/product/ProductRemoteService.java
@@ -1,17 +1,15 @@
 package me.wellstone.msa.product;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RequiredArgsConstructor
 public class ProductRemoteService {
     private static final String url = "http://product-service/products/";
     private final RestTemplate restTemplate;
-
-    public ProductRemoteService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
 
     @HystrixCommand(fallbackMethod = "getProductInfoFallback")
     public String getProductInfo(String productId) {
